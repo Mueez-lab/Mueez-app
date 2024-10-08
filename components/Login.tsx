50a8fd89ec065bd56ca132880fbf96b3f00a91fc
Mueez <mueezzakir6@gmail.com>
2024-10-08 15:53:08 +0500

    login modified

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,54 +1,75 @@
-import { View, Text, StyleSheet, TextInput, TouchableWithoutFeedback,Keyboard, TouchableOpacity, Pressable } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity, Pressable } from 'react-native'
 import Entypo from '@expo/vector-icons/Entypo';
 import Feather from '@expo/vector-icons/Feather';
 import { Switch } from 'react-native-paper';
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+
     const [isSwitchOn, setIsSwitchOn] = React.useState(false);
     const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+    const [isPressed, setIsPressed] = useState(false);  // State for press 
+
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.container}>
-            <View style={styles.innerContainer}>
-                <View style={styles.header}>
-                    <Text style={{fontSize:15, color:"#3f403f"}}>Nivara</Text>
-                    <View>
-                        <Text style={{fontSize:15}}>Sign up</Text>
+            <View style={styles.container}>
+                <View style={styles.innerContainer}>
+                    <View style={styles.header}>
+                        <Text style={{ fontSize: 25, color: "#3f403f", fontWeight: "400" }}>Nivara</Text>
+                        <TouchableOpacity activeOpacity={0.4}>
+                            <Text style={{ fontSize: 16, fontWeight: "500" }}>Sign up</Text>
+                        </TouchableOpacity>
                     </View>
-                </View>
-                <View style={styles.login}>
-                    <Text style={{fontSize:31}}>Log in</Text>
-                    <Pressable style={{flexDirection:"row", gap:5, justifyContent:"center",alignItems:"center", borderWidth:1, padding:10, borderRadius:20}}>
-                        <Entypo name="facebook-with-circle" size={24} color="black" />
-                        <Text>Facebook</Text>
-                    </Pressable>
-                </View >
-                <View style={{padding:10}}>
-                    <View style={{borderRadius:20, gap:20, width:"100%"}}>
-                        <View style={{justifyContent:"center", alignItems:"center",flexDirection:"row", backgroundColor:"rgba(245, 242, 273, 0.91)", padding:10, borderRadius:25}}>
-                            <View style={{ borderRadius:40, borderWidth:1,backgroundColor:"white", width:30, height:30, justifyContent:"center", alignItems:"center"}}>
-                                <Text style={{fontSize:20}}>@</Text>
+                    <View style={styles.login}>
+                        <TouchableOpacity>
+                        <Text style={{ fontSize: 27 }}>Log in</Text>
+                    </TouchableOpacity>
+                        <Pressable
+                            onPressIn={() => setIsPressed(true)}
+                            onPressOut={() => setIsPressed(false)}
+                            style={[
+                                { flexDirection: "row", gap: 5, justifyContent: "center", alignItems: "center", borderWidth: 1, padding: 10, borderRadius: 20 },
+                                { backgroundColor: isPressed ? 'black' : 'transparent' }
+                            ]}
+                        >
+                            <Entypo
+                                name="facebook-with-circle"
+                                size={24}
+                                color={isPressed ? 'white' : 'black'} // Change icon color
+                            />
+                            <Text style={{ color: isPressed ? 'white' : 'black' }}>Facebook</Text>
+                        </Pressable>
+                    </View >
+                    <View style={{ padding: 10 }}>
+                        <View style={{ borderRadius: 20, gap: 20, width: "100%" }}>
+                            <View style={{ justifyContent: "center", alignItems: "center", flexDirection: "row", backgroundColor: "rgba(245, 242, 273, 0.91)", padding: 10, borderRadius: 25 }}>
+                                <View style={{ borderRadius: 40, borderWidth: 1, width: 30, height: 30, justifyContent: "center", alignItems: "center" }}>
+                                    <Text style={{ fontSize: 20 }}>@</Text>
+                                </View>
+                                <TextInput style={{ width: "90%", padding: 10 }} placeholder='e-mail address' placeholderTextColor={"#5e5e5d"} />
                             </View>
-                            <TextInput style={{width:"90%", padding:10}} placeholder='e-mail address' placeholderTextColor={"black"}/>
-                        </View>
-                        <View style={{justifyContent:"center", alignItems:"center",flexDirection:"row", backgroundColor:"rgba(245, 242, 273, 0.91)",padding:10, borderRadius:25}}>
-                            <Feather name="key" size={24} color="black" />
-                            <TextInput style={{width:"90%", padding:10}} placeholder='password' placeholderTextColor={"black"}/>
-                            <View style={{backgroundColor:"white",position:"absolute", right:10, padding:10, borderRadius:25}}>
-                                <Text>I Forgot</Text>
+                            <View>
+                                <View style={{ justifyContent: "center", alignItems: "center", flexDirection: "row" ,backgroundColor: "rgba(245, 242, 273, 0.91)", paddingVertical: 10, borderRadius: 25,gap:5, paddingHorizontal:15 }}>
+                                    <Feather name="key" size={24} color="black" />
+                                    <TextInput style={{ flexGrow:1, padding: 10}} placeholder='password' placeholderTextColor={"#5e5e5d"} />
+                                    <View style={{alignItems:"center"}}>
+                                    <Entypo name="eye" size={24} color="black"  />
+                                    </View>
+                                </View>
+                                <TouchableOpacity  activeOpacity={0.5} style={{alignSelf:"flex-end", paddingRight:20, paddingTop:10}}>
+                                    <Text>Forget</Text>
+                                </TouchableOpacity>
                             </View>
                         </View>
                     </View>
-                </View>
-                <View style={styles.terms}>
-                    <View style={{width:"80%"}}>
-                        <Text>By logging in, you agree to our Terms of Service and Privacy Policy. Please agree to continue</Text>
+                    <View style={styles.terms}>
+                        <View style={{ width: "80%" }}>
+                            <Text>By logging in, you agree to our Terms of Service and Privacy Policy. Please agree to continue</Text>
+                        </View>
+                        <Switch value={isSwitchOn} color='black' onValueChange={onToggleSwitch} />
                     </View>
-                    <Switch value={isSwitchOn} color='black' onValueChange={onToggleSwitch} />
                 </View>
             </View>
-        </View>
         </TouchableWithoutFeedback>
     )
 }
@@ -60,29 +81,29 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(245, 242, 273, 0.57)',
         justifyContent: "center",
         alignItems: "center",
-        borderRadius:30,
-        padding:30
+        borderRadius: 30,
+        padding: 30
     },
     innerContainer: {
         width: "100%"
     },
     header: {
-        marginTop:10,
-        marginBottom:30,
-        paddingHorizontal:15,
+        marginTop: 10,
+        marginBottom: 30,
+        paddingHorizontal: 15,
         flexDirection: "row",
         justifyContent: "space-between",
     },
     login: {
-        paddingHorizontal:10,
+        paddingHorizontal: 10,
         flexDirection: "row",
         justifyContent: "space-between",
-        marginBottom:20
+        marginBottom: 20
     },
-    terms:{
-        marginTop:20,
-        paddingLeft:15,
-        flexDirection:"row",
-        marginBottom:10,
+    terms: {
+        marginTop: 20,
+        paddingLeft: 15,
+        flexDirection: "row",
+        marginBottom: 10,
     }
 })
